ca0067e4169fa10e89a644bb0f98c7cdba1492ab
ralexale <ralexale@gmail.com>
2024-09-16 20:59:23 -0500

    feat: create DTOs for taks entity

---
+++ todoList/DTOs/Request/TaskRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace todoList.DTOs.Request
+{
+    public class TaskRequest
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public TaskStatus status { get; set; }
+    }
+}
---
+++ todoList/DTOs/Response/TaskResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace todoList.DTOs.Response
+{
+    public class TaskResponse
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public TaskStatus status { get; set; }
+    }
+}
