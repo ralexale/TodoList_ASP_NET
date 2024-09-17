18b63c52c9cd1c6ec93a8e91f108aec3ea791285
ralexale <ralexale@gmail.com>
2024-09-16 20:38:37 -0500

    Add project files.

---
+++ todoList/Models/Tasks.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace todoList.Models
+{
+    public class Tasks
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Title {  get; set; }
+
+        public string Description { get; set; }
+
+        public bool status { get; set; }
+    }
+}
